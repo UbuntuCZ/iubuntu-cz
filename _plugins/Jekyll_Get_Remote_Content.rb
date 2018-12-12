662532d1473553aed4680fd36a026c53f5340515
Michal Stanke <michal@stanke.cz>
2018-12-12 08:11:13 +0100

    Zrušeno nepotřebné stahování souborů do `remote_content`

--- _plugins/Jekyll_Get_Remote_Content.rb
+++ _plugins/Jekyll_Get_Remote_Content.rb
@@ -5,26 +5,6 @@ require 'feedparser'
 
 module Jekyll_Get_Remote_Content
 
-  class Fetcher
-    def self.fetchFor(site)
-      config = site.config['remote_content']
-      if !config
-        return
-      end
-      if !config.kind_of?(Array)
-        config = [config]
-      end
-      Dir.mkdir('remote_content') unless Dir.exist?('remote_content')
-      config.each do |remote|
-        open(remote['url'], 'r', :allow_redirections => :all) do |remote_content|
-          open('remote_content/'+remote['name']+'.xml', 'wb') do |local_file|
-            local_file.write(remote_content.read)
-          end
-        end
-      end
-    end
-  end
-
   class FeedFetcher
     def self.fetchFor(site)
       config = site.config['remote_content']
@@ -42,10 +22,6 @@ module Jekyll_Get_Remote_Content
         end
       end
     end
-  end
-
-  Jekyll::Hooks.register :site, :after_reset do |site|
-    Fetcher.fetchFor(site)
   end
 
   Jekyll::Hooks.register :site, :post_read do |site|
